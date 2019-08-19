77c6f4113b578556825353a8ff5a0204b5f149a1
WilsontheWolf <33164598+WilsontheWolf@users.noreply.github.com>
2019-08-19 14:44:30 -0600

    1.0.1-dev
    added config support!
    rn now in game method

--- src/me/WilsontheWolf/JoinPlus/main.java
+++ src/me/WilsontheWolf/JoinPlus/main.java
@@ -1,9 +1,12 @@
 package me.WilsontheWolf.JoinPlus;
 
+import java.util.logging.Logger;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Server;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,17 +17,26 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class main extends JavaPlugin implements Listener {
 	Server server = Bukkit.getServer();
 	ConsoleCommandSender console = server.getConsoleSender();
-
+	Logger log = Bukkit.getLogger();
+	FileConfiguration config = this.getConfig();
 	@Override
 	public void onEnable()
 	{
 		getServer().getPluginManager().registerEvents(this, this);
 		console.sendMessage("Thank you for using"+ChatColor.GOLD+ChatColor.BOLD+ChatColor.UNDERLINE+" Join+");
+		this.saveDefaultConfig();
+		console.sendMessage(parseColor(config.getString("joinmsg"), "username"));
 	}
 	@Override
 	public void onDisable()
 	{
-		console.sendMessage(ChatColor.DARK_RED+"Note: Join+ has been disabled and your chat messages will now be the defaults!");
+		try{
+			config.options().copyDefaults(true);
+			saveConfig();
+		} catch (Exception e) {
+			log.warning("Join+ had an error saving the config!");
+			log.warning(e.toString());
+		}
 	}
 	
 
@@ -32,7 +44,7 @@ public class main extends JavaPlugin implements Listener {
 	  public void onJoin(PlayerJoinEvent event) {
 		  Player player= event.getPlayer();
 		  event.setJoinMessage(null);
-		  String message = ChatColor.BOLD+""+ChatColor.GOLD+"["+ChatColor.DARK_GREEN+"+"+ChatColor.GOLD+"]"+ChatColor.RESET+" "+ChatColor.DARK_AQUA+player.getDisplayName()+ChatColor.DARK_PURPLE+" has joined the game!";
+		  String message = parseColor(config.getString("joinmsg"), player.getDisplayName());
 		  Bukkit.broadcastMessage(message);
 
 	  }
@@ -41,8 +53,33 @@ public class main extends JavaPlugin implements Listener {
 	  public void onLeave(PlayerQuitEvent event) {
 		  Player player= event.getPlayer();
 		  event.setQuitMessage(null);
-		  String message = ChatColor.BOLD+""+ChatColor.GOLD+"["+ChatColor.DARK_RED+"-"+ChatColor.GOLD+"]"+ChatColor.RESET+" "+ChatColor.DARK_AQUA+player.getDisplayName()+ChatColor.DARK_PURPLE+" has left the game!";
+		  String message = parseColor(config.getString("leavemsg"), player.getDisplayName());
 		  Bukkit.broadcastMessage(message);
-
 	  }
+	  
+	  public String parseColor(String str , String name) {
+		  str = str.replace("{player}", name);
+		  str = str.replace("{aqua}", ChatColor.AQUA.toString());
+		  str = str.replace("{black}", ChatColor.BLACK.toString());
+		  str = str.replace("{blue}", ChatColor.BLUE.toString());
+		  str = str.replace("{dark_aqua}", ChatColor.DARK_AQUA.toString());
+		  str = str.replace("{dark_blue}", ChatColor.DARK_BLUE.toString());
+		  str = str.replace("{dark_gray}", ChatColor.DARK_GRAY.toString());
+		  str = str.replace("{dark_green}", ChatColor.DARK_GREEN.toString());
+		  str = str.replace("{dark_purple}", ChatColor.DARK_PURPLE.toString());
+		  str = str.replace("{dark_red}", ChatColor.DARK_RED.toString());
+		  str = str.replace("{gold}", ChatColor.GOLD.toString());
+		  str = str.replace("{gray}", ChatColor.GRAY.toString());
+		  str = str.replace("{green}", ChatColor.GREEN.toString());
+		  str = str.replace("{light_purple}", ChatColor.LIGHT_PURPLE.toString());
+		  str = str.replace("{red}", ChatColor.RED.toString()); 
+		  str = str.replace("{white}", ChatColor.WHITE.toString());
+		  str = str.replace("{yellow}", ChatColor.YELLOW.toString());
+		  str = str.replace("{bold}", ChatColor.BOLD.toString());
+		  str = str.replace("{italic}", ChatColor.ITALIC.toString());
+		  str = str.replace("{strikethrough}", ChatColor.STRIKETHROUGH.toString());
+		  str = str.replace("{underline}", ChatColor.UNDERLINE.toString());
+		  str = str.replace("{reset}", ChatColor.RESET.toString());
+		  return(str);
+		  }
 }
